57bf8c1e9ca337d8c72017111340adb3abb5bac0
tonyakolb <antoninakolb@gmail.com>
2026-03-11 18:05:02 +0300

    fix server port for render deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -59,7 +59,7 @@ app.get('/users', async (req, res) => {
   res.json(users);
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
