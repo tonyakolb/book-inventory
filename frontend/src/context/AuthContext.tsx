041f5ff53a11ff5f636c5ce33255d5e03ee4e72c
tonyakolb <antoninakolb@gmail.com>
2026-03-11 19:04:08 +0300

    fix 2

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
 
 type User = {
@@ -18,6 +19,7 @@ const AuthContext = createContext<AuthContextType | null>(null);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
+  const navigate = useNavigate();
 
   const login = (token: string) => {
     localStorage.setItem("token", token);
@@ -27,6 +29,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   const logout = () => {
     localStorage.removeItem("token");
     setUser(null);
+    navigate("/login")
   };
 
   const loadUser = async () => {
