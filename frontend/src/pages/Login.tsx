cea600494ec99e5eed505eeff65934c3c12be45a
tonyakolb <antoninakolb@gmail.com>
2026-03-12 15:07:01 +0300

    fix 10

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -68,14 +68,14 @@ export default function Login() {
 
       <a
         className="btn btn-outline-danger w-100 mb-2"
-        href="https://book-inventory-1-53wg.onrender.com/auth/google"
+        href="https://book-inventory-sp6n.onrender.com/auth/google"
       >
         Login with Google
       </a>
 
       <a
         className="btn btn-outline-dark w-100"
-        href="https://book-inventory-1-53wg.onrender.com/auth/github"
+        href="https://book-inventory-ssp6n.onrender.com/auth/github"
       >
         Login with GitHub
       </a>
